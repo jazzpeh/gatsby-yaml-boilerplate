8f526f13d41dc35e42413edad3224dd073c5661d
Jazz Peh <jazzpehz@gmail.com>
2019-08-01 21:45:47 +0800

    Update gatsby-node.js
    Updated support for yaml pages

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,3 +5,32 @@
  */
 
 // You can delete this file if you're not using it
+const path = require('path');
+
+exports.createPages = ({ actions, graphql }) => {
+  const { createPage } = actions;
+
+  const component = path.resolve('src/components/layout.js');
+
+  return graphql(`
+    {
+      allDataYaml {
+        edges {
+          node {
+            path
+          }
+        }
+      }
+    }
+  `).then((result) => {
+    if (result.errors) return Promise.reject(result.errors);
+
+    return result.data.allDataYaml.edges.forEach(({ node }) => {
+      createPage({
+        path: node.path,
+        component,
+        context: {}, // additional data can be passed via context
+      });
+    });
+  });
+};
